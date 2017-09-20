404b14fcfe7cf8b2c2dcb3a954c9a61e49be7422
aconigle <aconigle@iupui.edu>
2017-09-20 17:00:05 -0400

    added functionality
    added cards. added button functionality. added click listenter to button.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,45 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    cards = [
+        {
+            imgPath: "travel2.jpg",
+            title: "Travel Destination",
+            desc: "This is my new travel destination",
+            favorited: false
+        },
+        {
+            imgPath: "travel3.jpg",
+            title: "Travel Destination",
+            desc: "This is my exciting new travel destination",
+            favorited: false
+        },
+        {
+            imgPath: "travel4.jpg",
+            title: "Travel Destination",
+            desc: "This is my crazy new travel destination",
+            favorited: false
+        }
+    ];
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
 
-  }
+    }
 
+    clickFav(card) {
+//        gets index value of card
+        let index = this.cards.indexOf(card);
+        if (index > -1) {
+            if (card.favorited) {
+                this.cards[index].favorited = false;
+            } else {
+                this.cards[index].favorited = true;
+            }
+        }
+    }
 }
